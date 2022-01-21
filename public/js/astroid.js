a4bba7e7b87b63ffef2527e449fe07289d21e988
c13 <chris13ryant@gmail.com>
2022-01-21 10:58:40 -0700

    added astroid rotation and forced more grey color

--- public/js/astroid.js
+++ public/js/astroid.js
@@ -6,18 +6,20 @@ class Astroid {
         this.isInWindow = "spawn";
         this.direction = p5.Vector.sub(this.direction, this.location);
         this.speed = random(.01,10);
-        this.r = random(1,10);
+        this.r = random(1,20);
         this.numBlobs = random(100);
         this.atCamera = random([-1, 0]);
+        this.rotation = 0;
+        this.rotationSpeed = random(-.2, .2);
 
         this.body = createGraphics(this.r*2, this.r*2);
         push();
         this.body.noStroke();
-        this.body.fill(255*random());
+        //this.body.fill(255*random()-100);
         //this.body.ellipse(this.r, this.r, this.r*2, this.r*2);
         this.body.translate(this.r, this.r);
         for (let i = 0; i < this.numBlobs; i++){
-            this.body.fill(255*random());
+            this.body.fill(map(random(), 0, 1, 50, 100));
             let tempr = random();
             this.body.ellipse((this.r/2*random()*random([1, -1])), (this.r/2*random()*random([1, -1])), this.r*tempr/2, this.r*tempr/2);
         }
@@ -27,7 +29,14 @@ class Astroid {
 
     show() {
         
-        image(this.body, this.location.x, this.location.y, this.r*2, this.r*2);
+        push();
+        translate(this.location.x, this.location.y);
+        rotate(this.rotation);
+        //translate(this.location.x, this.location.y);
+        this.rotation += this.rotationSpeed;
+        imageMode(CENTER)
+        image(this.body, 0, 0, this.r*2, this.r*2);
+        pop();
         this.r = Math.max(.01, this.r + (.1*random())*this.atCamera);
         
         //ellipse(this.location.x, this.location.y, this.r*2, this.r*2);
